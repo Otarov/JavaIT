26f8d70e7ff33b03f838aef2fce6549e69e8a2e3
Rasul <ibn-musa@ro.ru>
2016-03-07 16:19:19 +0300

    feat: Prostye Chisla

---
+++ RasulProject/src/ProstyeChisla.java
@@ -0,0 +1,25 @@
+
+public class ProstyeChisla {
+
+	public static void main(String[] args) {
+		System.out.println("Все простые числа от 0 до 1000");
+		
+		for (int i = 1; i <= 1000; i++){ 	// перебор всех чисел от 1 до 1000
+			
+			//System.out.println("проверка числа " + i);
+			
+			boolean prostoe = true;			
+			int j = 2;
+			//System.out.println(prostoe);
+			while ((prostoe) && (j < i/2+1 )) {
+				
+				if (i % j == 0) prostoe = false;
+				else j++;// 
+			}
+			//System.out.println(prostoe);
+			if (prostoe) System.out.println(i);
+		}
+
+	}
+
+}
