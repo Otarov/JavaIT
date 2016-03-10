5f162201a8711b859f903b4d016aebf364769020
Rasul <ibn-musa@ro.ru>
2016-03-11 00:27:46 +0300

    +++

--- RasulProject/src/HomeWork_2/Pet.java
+++ RasulProject/src/HomeWork_2/Pet.java
@@ -2,9 +2,9 @@ package HomeWork_2;
 
 public abstract class Pet { 	// ќбщий класс домашних животных, у любого из которых есть:
 
-	String owner;				// владелец,
-	int age;					// возраст.
-	boolean alive;				// —осто€ние может жив (true) или нет (false)
+	private String owner;				// владелец,
+	private int age;					// возраст.
+	private boolean alive;				// —осто€ние: жив (true) или нет (false)
  	
 	
 	public String getOwner() {				// метод, позвол€ющий вызвать владельца
@@ -17,7 +17,8 @@ public abstract class Pet { 	// ќбщий класс домашних животных, у любого из котор
 		return age;
 	}
 	public void setAge(int age) {
-		this.age = age;
+		if (age >= 0)	this.age = age;
+		else System.out.println("¬озраст не может быть отрицательным!"); 
 	}
 	
 	public boolean isAlive() {				// и его жизнеспособности
@@ -33,12 +34,13 @@ public abstract class Pet { 	// ќбщий класс домашних животных, у любого из котор
 	public abstract void voice();	// пустой абстрактный метод издавать звуки, а какие именно,
 									// и даже какого рода эти звуки, пока не известно.
 	
-	public int beOld(int age){				// —тарение на год
-		return ++age;
+	public int beOld(int age){				// —тарение на год, если он жив пока
+		if (alive) age++;
+		return age;
 	}
 	
-	public int beOld(int age, int k){		// —тарение на k лет
-		age +=k;
+	public int beOld(int age, int k){		// —тарение на k лет, если пока жив
+		if (alive) age +=k;
 		return age;
 	}
 	
