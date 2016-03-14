e7fd646d3bb6ee6e00b44ddeeb19dd75859063bd
Rasul <ibn-musa@ro.ru>
2016-03-14 11:45:45 +0300

    feat:

---
+++ RasulProject/src/matrix/Matrix.java
@@ -0,0 +1,98 @@
+package matrix;
+
+public class Matrix {			// общий класс двумерных целочисленных массивов (матриц)
+
+								// размерность
+	private int n;				// кол-во строк
+	private int m;				// кол-во столбцов
+	
+	private int[][] array;		// сама матрица
+	
+	
+	public int getN() {			// геттеры - сеттеры...
+		return n;
+	}
+
+	public void setN(int n) {
+		this.n = n;
+	}
+
+	public int getM() {
+		return m;
+	}
+
+	public void setM(int m) {
+		this.m = m;
+	}
+
+	public int[][] getArray() {
+		return array;
+	}
+
+	public void setArray(int[][] array) {
+		this.array = array;
+	}
+
+	
+	
+
+	public Matrix(int n, int m, int[][] array) {	// конструктор, иниц-щий матрицу со всеми параметрами
+		super();
+		this.n = n;
+		this.m = m;
+		this.array = array;
+	}
+	
+	public Matrix(int n, int m) {		// матрица по умолчанию n х m и с элементами, равными сумме индексов
+		super();
+		this.n = n;
+		this.m = m;
+		int[][] arr = new int[n][m];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				arr[i][j] = i + j;
+		setArray(arr);
+	}
+	
+	public Matrix() {		// матрица по умолчанию 3 х 3 и с элементами, равными сумме индексов
+		super();			// не понял, что это делает
+		this.n = 3;
+		this.m = 3;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				array[i][j] = i + j;
+			
+	}
+	
+	public void matrixPrint() {		// так наша матрица выводится на экран
+		System.out.println("Двумерный массив размером " + n + " x " + m + ":");
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < m; j++){
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public Matrix matrixSumm(Matrix arr1) {		// сумма двух матриц одинаковой размерности
+		Matrix summ = new Matrix();
+			System.out.println("Сумма двух матриц " + arr1.n + " x " + arr1.m + ":");
+		
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				summ.array[i][j] = arr1.array[i][j] + array[i][j];
+		return summ;	
+		}
+	
+	public Matrix matrixRazn(Matrix arr1) {		// разность двух матриц одинаковой размерности
+		Matrix summ = new Matrix();
+			System.out.println("Разность двух матриц " + arr1.n + " x " + arr1.m + ":");
+		
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				summ.array[i][j] = arr1.array[i][j] - array[i][j];
+		return summ;	
+		}
+	
+	
+}
---
+++ RasulProject/src/matrix/MatrixTest.java
@@ -0,0 +1,47 @@
+package matrix;
+
+public class MatrixTest {
+
+	
+	
+	public static void main(String[] args) {
+	
+		int[][] arr = new int[3][5];
+		arr[0][0] = 1;
+		arr[0][1] = 3;
+		arr[0][2] = 5;
+		arr[0][3] = 6;
+		arr[0][4] = 8;
+		arr[1][0] = 4;
+		arr[1][1] = -3;
+		arr[1][2] = 0;
+		arr[1][3] = 11;
+		arr[1][4] = 3;
+		arr[2][0] = 4;
+		arr[2][1] = 2;
+		arr[2][2] = 8;
+		arr[2][3] = 44;
+		arr[2][4] = 8;
+		
+
+		Matrix array1 = new Matrix(3, 5, arr);
+		
+		Matrix array2 =new Matrix(3,5);
+		
+		System.out.println("Первая матрица, созданная вручную");
+	
+		array1.matrixPrint();
+		
+		System.out.println("Вторая матрица, созданная по умолчанию при вводе только размерности");
+		
+		array2.matrixPrint();
+		Matrix arraySumm = array1.matrixSumm(array2);
+		System.out.println("Сумма двух матриц:");
+		arraySumm.matrixPrint();
+		
+		
+		// TODO Auto-generated method stub
+		}
+	}
+
+
